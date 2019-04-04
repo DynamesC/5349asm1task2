87acada80a5f4eaeecff3f598d94ff83ab6eece7
陈泽洲 <zche7701@uni.sydney.edu.au>
2019-04-04 18:08:21 +1100

    Update task2.py

--- task2.py
+++ task2.py
@@ -7,9 +7,86 @@
 #   --output outputfile
 
 from pyspark import SparkContext
-from task2_utils import *
+# from task2_utils import *
 from datetime import datetime
 import argparse
+
+def extractVideoInfo(record):
+    parts = record.strip().split(",")
+
+    if len(record) != 12 or parts[0] == "video_id":
+        return
+
+    video_id = parts[0]
+    country = parts[11]
+    date = parts[1]
+    likes = parts[6]
+    dislikes = parts[7]
+    category = parts[3]
+
+    return ("{}|{}".format(video_id, country), "{}|{}|{}|{}".format(date, likes, dislikes, category))
+
+def mergeInfo(accumulatedInfo, currentInfo):
+    date1, date2, date1_like, date1_dislike, date2_like, date2_dislike, category = accumulatedInfo
+
+    currentInfoParts = currentInfo.split("|")
+
+    if len(currentInfoParts != 4):
+        return accumulatedInfo
+
+    currentInfoDate = datetime.strptime(currentInfoParts[0] , '%Y-%m-%d')
+    currentInfoCategory = currentInfoParts[3]
+
+    try:
+        currentInfoLikes = int(currentInfoParts[1])
+        currentInfoDisLikes = int(currentInfoParts[2])
+    except:
+        return accumulatedInfo
+
+    if currentInfoDate < date1:
+        date2 = date1
+        date1 = currentInfoDate
+        date2_like = date1_like
+        date2_dislike = date1_dislike
+        date1_like = currentInfoLikes
+        date1_dislike = currentInfoDisLikes
+        category = currentInfoCategory
+
+    if date1 < currentInfoDate < date2:
+        date2 = currentInfoDate
+        date2_like = currentInfoLikes
+        date2_dislike = currentInfoDisLikes
+
+    return (date1, date2, date1_like, date1_dislike, date2_like, date2_dislike, category)
+
+
+def mergeInfoCombiner(accumulatedInfo1, accumulatedInfo2):
+    date11, date12, date11_like, date11_dislike, date12_like, date12_dislike, category1 = accumulatedInfo1
+    date21, date22, date21_like, date21_dislike, date22_like, date22_dislike, category2 = accumulatedInfo2
+
+    pair1 = (date11, date11_like, date11_dislike, category1)
+    pair2 = (date12, date12_like, date12_dislike, category1)
+    pair3 = (date21, date21_like, date21_dislike, category2)
+    pair4 = (date22, date22_like, date22_dislike, category2)
+
+    pairList = [pair1, pair2, pair3, pair4]
+    pairList.sort(key=lambda tup: tup[0])
+
+    smallestPair1 = pairList[0]
+    smallestPair2 = pairList[1]
+
+    return(smallestPair1[0], smallestPair2[0], smallestPair1[1], smallestPair1[2], smallestPair2[1], smallestPair2[2], smallestPair1[3])
+
+def mapResult(line):
+    key, value = line
+    date1, date2, date1_like, date1_dislike, date2_like, date2_dislike, category = accumulatedInfo
+
+    video_id = key.split("|")[0]
+    country = key.split("|")[1]
+
+    value = (date2_dislike - date1_dislike) - (date2_like - date1_like)
+
+    return (video_id, "{}|{}|{}".format(value, category, country))
 
 
 if __name__ == "__main__":
