2df0c24cc2449c3065d9643da9ffa887d553ba72
陈泽洲 <zche7701@uni.sydney.edu.au>
2019-04-04 20:37:45 +1100

    Update task2.py

--- task2.py
+++ task2.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
     video_data = sc.textFile(input_path + "AllVideos_short.csv")
 
     video_infos = video_data.map(extractVideoInfo)
-    result = video_infos.aggregateByKey((datetime.strptime('9999.09.09' , '%Y.%d.%m'), datetime.strptime('9999.09.09' , '%Y.%d.%m'), 0, 0, 0, 0, "Unknown"), mergeInfo, mergeInfoCombiner, 1 ).map(mapResult).sortBy(_._2)
+    result = video_infos.aggregateByKey((datetime.strptime('9999.09.09' , '%Y.%d.%m'), datetime.strptime('9999.09.09' , '%Y.%d.%m'), 0, 0, 0, 0, "Unknown"), mergeInfo, mergeInfoCombiner, 1 ).map(mapResult).sortBy(_._2, false)
     result.take(10).saveAsTextFile(output_path)
 
 
