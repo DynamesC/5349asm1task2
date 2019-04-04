466fdf332efcc51486a08f91fac55f0c7d50461f
陈泽洲 <zche7701@uni.sydney.edu.au>
2019-04-04 22:43:08 +1100

    Update task2.py

--- task2.py
+++ task2.py
@@ -106,10 +106,10 @@ if __name__ == "__main__":
     input_path = args.input
     output_path = args.output
     video_data = sc.textFile(input_path + "AllVideos_short.csv")
-
+    checkList = ["BEePFpC9qG8", "RmZ3DPJQo2k", "1Aoc-cd9eYs", "QwZT7T-TXT0"]
     video_infos = video_data.map(extractVideoInfo)
     result = video_infos.aggregateByKey((datetime.strptime('9999.09.09' , '%Y.%d.%m'), datetime.strptime('9999.09.09' , '%Y.%d.%m'), 0, 0, 0, 0, "Unknown"), mergeInfo, mergeInfoCombiner, 1 ).map(mapResult)
-    final = sc.parallelize(result.sortBy(lambda r: r[1], False).filter(lambda r: r[1] < 334594).take(10))
+    final = sc.parallelize(result.sortBy(lambda r: r[1], False).filter(lambda r: r[0] in checkList).take(10))
     final.saveAsTextFile(output_path)
     final.foreach(print)
     # test_record = "SbOwzAl9ZfQ,17.14.11,24,Entertainment,2017-11-13T06:06:22.000Z,310130,4182,361,1836,False,False,MX\n"
