e922b901b9e7099d46c4011d66d183a6ba36d859
陈泽洲 <zche7701@uni.sydney.edu.au>
2019-04-06 18:50:44 +1100

    Update task2.py

--- task2.py
+++ task2.py
@@ -8,7 +8,6 @@
 
 from pyspark import SparkContext
 from datetime import datetime
-import spark.implicits._
 import argparse
 
 """
@@ -139,7 +138,6 @@ if __name__ == "__main__":
     result = video_infos.aggregateByKey((datetime.strptime('9999.09.09' , '%Y.%d.%m'), datetime.strptime('9999.09.09' , '%Y.%d.%m'), 0, 0, 0, 0, "Unknown"), mergeInfo, mergeInfoCombiner, 1 ).map(mapResult)
     final = sc.parallelize(result.sortBy(lambda r: r[1], False).take(10))
     final.saveAsTextFile(output_path)
-    final.toDF().show()
     # test_record = "SbOwzAl9ZfQ,17.14.11,24,Entertainment,2017-11-13T06:06:22.000Z,310130,4182,361,1836,False,False,MX\n"
     # test_extracted_record = extractVideoInfo(test_record)
     # print(test_extracted_record)
