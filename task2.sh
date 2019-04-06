1d809d499380c2e32e054c74f2f6de177adddd28
陈泽洲 <zche7701@uni.sydney.edu.au>
2019-04-06 18:44:12 +1100

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -1,5 +1,12 @@
+if [ $# -ne 2 ]; then
+    echo "Invalid number of parameters!"
+    echo "Usage: ./tag_driver.sh [input_location] [output_location]"
+    exit 1
+fi
+
+
 spark-submit  \
 --master local[2] \
 task2.py
-#   --input input-path
-#   --output outputfile
+--input $1
+--output $1
